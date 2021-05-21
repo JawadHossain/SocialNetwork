53920c0db58cbbfcafca8582ea4f9c41ec8c82be
Jawad Hossain <jawad.hossain@ibm.com>
2021-05-20 18:22:06 -0600

    Add Post Model, PostService and Observable

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from 'rxjs';
+
+import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  // posts = [
-  //   {title: 'First Post', content: "This is the first post's content"},
-  //   {title: 'Second Post', content: "This is the second post's content"},
-  //   {title: 'Third Post', content: "This is the third post's content"}
-  // ]
-  posts = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  private postsSub: Subscription;
+  posts: Post[] = [];
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit(): void {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+      .subscribe( (posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.postsSub.unsubscribe(); // remove listener
+  }
 }
